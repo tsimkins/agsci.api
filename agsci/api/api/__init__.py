05ce90fabecf24b8d6ebe111dbdb6109035d06e2
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-03-29 13:40:52 -0400

    Removed additional fields for non-products

--- agsci/api/api/__init__.py
+++ agsci/api/api/__init__.py
@@ -361,7 +361,8 @@ class BaseView(BrowserView):
 
         else:
             # Remove all the product fields for non-products
-            for k in ('publish_date', 'product_expiration', 'updated_at', 'plone_status', 'language'):
+            for k in ('publish_date', 'product_expiration', 'updated_at', 
+                      'plone_status', 'language', 'authors', 'contacts', 'owners'):
                 if data.has_key(k):
                     del data[k]
 
